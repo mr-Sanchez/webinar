39d7eb8b499c2aca949de880eec6f394cc9dc096
Travov Alexander <aleksandr.ptrk@gmail.com>
2021-07-16 22:03:35 +0300

    added model UserFile

--- webinar/assistant/models.py
+++ webinar/assistant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserFile(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    original_name = models.CharField(max_length=255)
+    path = models.FileField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+    shared_file = models.BooleanField(default=False)
